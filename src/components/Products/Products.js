c21e75fd7a86e49980964c97ab91fb8bcb2e15d4
Ayman Omer <aymangebrilomer@gmail.com>
2021-02-09 23:47:22 -0500

    disply prodacts to screen on the menu page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Product from "./Product/Product";
 
-const Products = () => {
-  return <div></div>;
+const Products = ({ products }) => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => {
+          return (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Product product={product} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </main>
+  );
 };
 
 export default Products;
