93df275cc4b2d1973241bb6029091446be8e9ed0
Shafee <shafeelinks@gmail.com>
2021-02-10 08:35:58 -0500

    footer had an upgrade

--- src/components/Community.js
+++ src/components/Community.js
@@ -18,7 +18,7 @@ const Community = () => {
 									d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
 								/>
 							</svg>
-							Cultivated With Love
+							Cultivated with Love
 						</h1>
 						<p className="mb-2 text-center">Garamcha teas are gathered by beautiful people from a beautiful land.</p>
 					</div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,39 +2,42 @@ import React from "react";
 
 export const Content = () => {
 	return (
-		<div class="min-w-screen min-h-screen bg-primary flex items-center p-5 lg:p-10 overflow-hidden relative">
-			<div class="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
-				<div class="md:flex items-center -mx-10">
-					<div class="w-full md:w-1/2 px-10 mb-10 md:mb-0">
-						<div class="relative">
+		<div className="min-w-screen min-h-screen bg-primary flex items-center p-5 lg:p-10 overflow-hidden relative">
+			<div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+				<div className="md:flex items-center -mx-10">
+					<div className="w-full md:w-1/2 px-10 mb-10 md:mb-0">
+						<div className="relative">
 							<img
 								src="https://images.pexels.com/photos/641038/pexels-photo-641038.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-								class="w-full relative z-10"
+								className="w-full relative z-10"
 								alt=""
 							/>
 						</div>
 					</div>
-					<div class="w-full md:w-1/2 px-10">
-						<h1 class="font-bold uppercase text-2xl mb-5">Featured</h1>
-						<div class="mb-10">
-							<h1 class="text-2xl mb-5">Bengal Mountain Tea</h1>
-							<p class="text-sm text-justify">
+					<div className="w-full md:w-1/2 px-10">
+						<h1 className="font-bold uppercase text-2xl mb-5">Featured</h1>
+						<div className="mb-10">
+							<h1 className="text-2xl mb-5">Bengal Mountain Tea</h1>
+							<p className="text-sm text-justify">
 								Our new featured item, the Bengal Mountain Tea, is a unique blend of tea made with herbs found solely in the mountaneous
 								regions of Srimangal, Sylhet. This tea is slightly bitter with earthy tones, and promotes healthy digestion.&nbsp;&nbsp;
-								<a href="#" class="opacity-50 text-gray-900 hover:opacity-100 inline-block text-xs leading-none border-b border-gray-900">
-									See more <i class="mdi mdi-arrow-right"></i>
+								<a
+									href="#"
+									className="opacity-50 text-gray-900 hover:opacity-100 inline-block text-xs leading-none border-b border-gray-900"
+								>
+									See more <i className="mdi mdi-arrow-right"></i>
 								</a>
 							</p>
 						</div>
 						<div>
-							<div class="inline-block align-bottom mr-5">
-								<span class="text-2xl leading-none align-baseline">$</span>
-								<span class="font-bold text-5xl leading-none align-baseline">29</span>
-								<span class="text-2xl leading-none align-baseline">.99</span>
+							<div className="inline-block align-bottom mr-5">
+								<span className="text-2xl leading-none align-baseline">$</span>
+								<span className="font-bold text-5xl leading-none align-baseline">29</span>
+								<span className="text-2xl leading-none align-baseline">.99</span>
 							</div>
-							<div class="inline-block align-bottom">
-								<button class="bg-primary opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
-									<i class="mdi mdi-cart -ml-2 mr-2"></i> ORDER NOW
+							<div className="inline-block align-bottom">
+								<button className="bg-primary opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
+									<i className="mdi mdi-cart -ml-2 mr-2"></i> ORDER NOW
 								</button>
 							</div>
 						</div>
