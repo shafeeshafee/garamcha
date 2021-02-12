b1627a29748bee826f5ccffcd9720120173196ca
Shafee <shafeelinks@gmail.com>
2021-02-11 21:35:26 -0500

    cart stays in one place

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,74 +5,57 @@ import { Link } from "react-router-dom";
 import CartItem from "./CartItem/CartItem";
 import useStyles from "./styles";
 
-const Cart = ({
-  cart,
-  handleUpdteCart,
-  handleRemoveFromCart,
-  handleEmptyCart,
-}) => {
-  const classes = useStyles();
-  const EmptyCart = () => (
-    <Typography variant="subtitle1">
-      You have no items in your shopping cart,
-      <Link className={classes.link} to="/menu">
-        start adding some
-      </Link>
-      !
-    </Typography>
-  );
-  if (!cart.line_items) return "Loading ...";
-  const FilledCart = () => (
-    <>
-      <Grid container spacing={3}>
-        {cart.line_items.map((item) => (
-          <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem
-              item={item}
-              onUpdateCartQty={handleUpdteCart}
-              onRemoveFromCart={handleRemoveFromCart}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <div className={classes.cardDetails}>
-        <Typography variant="h4">
-          Subtotal: {cart.subtotal.formatted_with_symbol}
-        </Typography>
-        <div>
-          <Button
-            className={classes.emptyButton}
-            size="large"
-            type="button"
-            variant="contained"
-            color="secondary"
-            onClick={handleEmptyCart}
-          >
-            Empty cart
-          </Button>
-          <Button
-            className={classes.checkoutButton}
-            size="large"
-            type="button"
-            variant="contained"
-            color="primary"
-          >
-            Checkout
-          </Button>
-        </div>
-      </div>
-    </>
-  );
+const Cart = ({ cart, handleUpdteCart, handleRemoveFromCart, handleEmptyCart }) => {
+	const classes = useStyles();
+	const EmptyCart = () => (
+		<Typography variant="subtitle1">
+			You have no items in your shopping cart,
+			<Link className={classes.link} to="/menu">
+				<span className="text-secondary ml-1">start adding some</span>
+			</Link>
+			!
+		</Typography>
+	);
+	if (!cart.line_items) return "Loading ...";
+	const FilledCart = () => (
+		<>
+			<Grid container spacing={3}>
+				{cart.line_items.map((item) => (
+					<Grid item xs={12} sm={4} key={item.id}>
+						<CartItem item={item} onUpdateCartQty={handleUpdteCart} onRemoveFromCart={handleRemoveFromCart} />
+					</Grid>
+				))}
+			</Grid>
+			<div className={classes.cardDetails}>
+				<Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
+				<div>
+					<Button
+						className={classes.emptyButton}
+						size="large"
+						type="button"
+						variant="contained"
+						color="secondary"
+						onClick={handleEmptyCart}
+					>
+						Empty cart
+					</Button>
+					<Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">
+						Checkout
+					</Button>
+				</div>
+			</div>
+		</>
+	);
 
-  return (
-    <Container>
-      <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3" gutterBottom>
-        Your Shopping Cart
-      </Typography>
-      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
-    </Container>
-  );
+	return (
+		<Container>
+			<div className={classes.toolbar} />
+			<Typography className={classes.title} variant="h3" gutterBottom>
+				Your Shopping Cart
+			</Typography>
+			{!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+		</Container>
+	);
 };
 
 export default Cart;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,46 +4,41 @@ import { IconButton, Badge } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 
 export const Navbar = ({ toggle, totalItems }) => {
-  const location = useLocation();
-  return (
-    <nav
-      className="font-headings bg-primary sticky top-0 flex justify-between items-center h-16 text-black shadow-sm header-font "
-      role="navigation"
-    >
-      <Link to="/" className="flex items-center pl-8">
-        <img src={logo} alt="" className="h-10 mx-2 " />
-        Garamcha
-      </Link>
-      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
-        <svg
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-      <div className="pr-8 xl:flex md:block hidden">
-        <Link className="hover:bg-secondary p-4" to="/">
-          Home
-        </Link>
-        <Link className="hover:bg-secondary p-4" to="/menu">
-          Menu
-        </Link>
-        <Link className="hover:bg-secondary p-4" to="/team">
-          Team
-        </Link>
-        <Link className="hover:bg-secondary p-4" to="/contact">
-          Contact
-        </Link>
-        {/* Shopping Cart Badge */}
+	const location = useLocation();
+	return (
+		<nav
+			className="font-headings bg-primary sticky top-0 flex justify-between items-center h-16 text-black shadow-sm header-font "
+			role="navigation"
+		>
+			<Link to="/" className="flex items-center pl-8">
+				<img src={logo} alt="" className="h-10 mx-2 " />
+				Garamcha
+			</Link>
+			<div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
+				<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<path
+						fillRule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div className="pr-8 xl:flex md:block hidden">
+				<Link className="hover:bg-secondary p-4" to="/">
+					Home
+				</Link>
+				<Link className="hover:bg-secondary p-4" to="/menu">
+					Menu
+				</Link>
+				<Link className="hover:bg-secondary p-4" to="/team">
+					Team
+				</Link>
+				<Link className="hover:bg-secondary p-4" to="/contact">
+					Contact
+				</Link>
+				{/* Shopping Cart Badge */}
 
-        {/* <Link className="p-4" to="/checkout">
+				{/* <Link className="p-4" to="/checkout">
 						<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 							<path
 								strokeLinecap="round"
@@ -53,19 +48,14 @@ export const Navbar = ({ toggle, totalItems }) => {
 							/>
 						</svg>
 					</Link> */}
-        {location.pathname === "/menu" && (
-          <IconButton
-            component={Link}
-            to="/cart"
-            aria-label="Show cart items"
-            color="inherit"
-          >
-            <Badge badgeContent={totalItems} color="secondary">
-              <ShoppingCart />
-            </Badge>
-          </IconButton>
-        )}
-      </div>
-    </nav>
-  );
+				{
+					<IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+						<Badge badgeContent={totalItems} color="secondary">
+							<ShoppingCart />
+						</Badge>
+					</IconButton>
+				}
+			</div>
+		</nav>
+	);
 };
