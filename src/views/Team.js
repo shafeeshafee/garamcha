de0b6cf4de81c63854a50457f953db13d1e7c007
Shafee <shafeelinks@gmail.com>
2021-02-13 23:22:21 -0500

    team page added with linkedin and github links

--- src/views/Team.js
+++ src/views/Team.js
@@ -1,28 +1,80 @@
 import React from "react";
 import Description from "../components/Description";
-import Storyline from "../components/Storyline";
+import IconButton from "@material-ui/core/IconButton";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const Team = () => {
 	const personQuote = (person) => {
 		switch (person) {
 			case "ayman":
 				return (
-					<p>
-						"We started this project knowing we would want to do something e-commerce related. But we also asked ourselves,
-						<em> what can we do to make this project helpful for others?</em> That's when Shafee and I thought about the small businesses
-						that are struggling today. Many of them don't have an online presence. Here with this project, we show that anyone can bring
-						their ideas to life."
-					</p>
+					<div className="text-justify">
+						<p>
+							"We started this project knowing we would want to do something e-commerce related. But we also asked ourselves,
+							<em> what can we do to make this project helpful for others?</em> That's when Shafee and I thought about the small businesses
+							that are struggling today. Many of them don't have an online presence. Here with this project, we show that anyone can bring
+							their ideas to life."
+						</p>
+						<div className="flex justify-around items-center">
+							<IconButton
+								selected
+								aria-label="LinkedIn"
+								onClick={() => window.open("https://www.linkedin.com/in/ayman-omer-b2429b1ab/")}
+								style={{ backgroundColor: "transparent" }}
+							>
+								<LinkedInIcon style={{ color: "#bd9d00", fontSize: "3.3rem", marginTop: "1em", textAlign: "center", cursor: "pointer" }} />
+							</IconButton>
+							<IconButton
+								selected
+								aria-label="GitHub"
+								onClick={() => window.open("https://github.com/aymanjebril2")}
+								style={{
+									backgroundColor: "transparent",
+									outline: "none",
+									border: "none",
+								}}
+							>
+								<GitHubIcon style={{ color: "#bd9d00", fontSize: "3rem", marginTop: "1em", textAlign: "center", cursor: "pointer" }} />
+							</IconButton>
+						</div>
+					</div>
 				);
 			case "shafee":
 				return (
-					<p>
-						"Growing up, I've always heard wonderful stories from my parents about that distant land they came from. Being born and raised
-						in the bustling city of New York, I felt a slight disconnect with my ethnic background. What was this land of green trees, white
-						waves, friendly faces, and tea? My studious teammate Ayman offered that we do something to shine a light on Bangladeshi culture.
-						I couldn't say no. This would be a homage to the country my parents left over 20 years ago. My mother helped us choose the name
-						Garamcha (literally:<em> warm tea)</em>."
-					</p>
+					<div className="text-justify">
+						<p>
+							"Growing up, I've always heard wonderful stories from my parents about that distant land they came from. Being born and raised
+							in the bustling city of New York, I felt a slight disconnect with my ethnic background. What was this land of green trees,
+							white waves, friendly faces, and tea? My studious teammate Ayman offered that we do something to shine a light on Bangladeshi
+							culture. I couldn't say no. This would be a homage to the country my parents left over 20 years ago. My mother helped us
+							choose the name Garamcha (literally:<em> warm tea)</em>."
+							<div className="flex justify-around items-center">
+								<IconButton
+									selected
+									aria-label="LinkedIn"
+									onClick={() => window.open("https://www.linkedin.com/in/shafeelinks")}
+									style={{ backgroundColor: "transparent" }}
+								>
+									<LinkedInIcon
+										style={{ color: "#bd9d00", fontSize: "3.3rem", marginTop: "1em", textAlign: "center", cursor: "pointer" }}
+									/>
+								</IconButton>
+								<IconButton
+									selected
+									aria-label="GitHub"
+									onClick={() => window.open("https://www.github.com/shafee")}
+									style={{
+										backgroundColor: "transparent",
+										outline: "none",
+										border: "none",
+									}}
+								>
+									<GitHubIcon style={{ color: "#bd9d00", fontSize: "3rem", marginTop: "1em", textAlign: "center", cursor: "pointer" }} />
+								</IconButton>
+							</div>
+						</p>
+					</div>
 				);
 			default:
 				return (
@@ -36,7 +88,7 @@ const Team = () => {
 
 	return (
 		<div>
-			<div className="center-content">
+			<div className="center-content text-center">
 				<div className="xl:flex xl:flex-row lg:flex-col">
 					<img
 						className="xl:w-1/2 lg:w-screen h-auto"
@@ -51,6 +103,7 @@ const Team = () => {
 						src="https://images.pexels.com/photos/2586339/pexels-photo-2586339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 						alt=""
 					/>
+
 					<Description name="Shafee Ahmed" title="Front End Engineer" text={personQuote("shafee")} />
 				</div>
 			</div>
