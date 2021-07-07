ec15922421b297e2bd3cc9eca154dd117d815b22
Shafee <shafeelinks@gmail.com>
2021-07-07 05:05:12 -0400

    testing if update worked

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-
+// testing
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
