a6f77caa7bcc2b25e8136c00f82e07752e996da2
Shafee <shafeelinks@gmail.com>
2021-07-07 05:42:12 -0400

    styling for products and cart items to be more responsive

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 const Subscribe = () => {
+	const handleForm = (e) => {
+		e.preventDefault();
+	};
 	return (
-		<div>
+		<div className="rounded-lg mx-auto p-2">
 			<hr />
 			<h3 className="font-headings p-3 text-xl text-center">Subscribe to our newsletter</h3>
-			<form class="m-4 flex justify-center">
+
+			<form className="m-4 flex justify-center">
 				<input
-					class="text-center rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white"
+					className="cursor-not-allowed text-center rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white focus:outline-none"
 					placeholder="your@mail.com"
 				/>
-				<button class="rounded-r-lg bg-primary  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">
+				<button
+					onClick={handleForm}
+					className="cursor-not-allowed rounded-r-lg bg-primary  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r focus:outline-none"
+				>
 					Subscribe
 				</button>
 			</form>
