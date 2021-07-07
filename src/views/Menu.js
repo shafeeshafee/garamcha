1ae1e6e34af4d98fe6ab3c79503169fefe4e7207
Shafee <shafeelinks@gmail.com>
2021-07-07 06:28:55 -0400

    added menu items for checking outside

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "../lib/commerce";
 import Products from "../components/Products/Products";
-import { useHistory } from "react-router";
 import Fade from "react-reveal/Fade";
 
 const Menu = ({ handleAdd }) => {
 	const [products, setProducts] = useState([]);
-	const history = useHistory();
 
 	const fetchProducts = async () => {
 		const { data } = await commerce.products.list();
@@ -16,12 +14,6 @@ const Menu = ({ handleAdd }) => {
 	useEffect(() => {
 		fetchProducts();
 	}, []);
-
-	// const user = localStorage.getItem("user-info");
-
-	// useEffect(() => {
-	// 	if (!user) history.push("/login");
-	// }, [history]);
 
 	return (
 		<>
